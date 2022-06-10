df451708bacfaf698e9ee54bc7babf70bbc17298
ali1sba <hariri.ali.official@gmail.com>
2022-06-10 12:23:49 +0100

    adding wilaya initHelper

--- src/routes/statisticRoutes.js
+++ src/routes/statisticRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const statisticController = require('../controllers/statisticController')
+const initHelper = require('../controllers/initHelper')
 
-router.post('/users', statisticController.getUserStatistic())
+router.get('/cities', statisticController.getCityStatistics)
+router.get('/Purchases', statisticController.getPurchaseStatistics)
+router.get('/categories', statisticController.getSubCategoryStatistic)
+router.get('/users', statisticController.getUserStatistic)
+router.post('/initwilaya', initHelper.add58wilaya)
 
 module.exports = router
