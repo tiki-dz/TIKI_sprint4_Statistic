9e05839b33169a652552f6046853b441ac3d80fe
nour <grebici.nour@gmail.com>
2022-06-14 17:13:52 +0100

    move to local db

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// const { sequelize } = require('./models')
+const { sequelize } = require('./models')
 // const createError = require("http-errors")
 const express = require('express')
 const path = require('path')
@@ -22,9 +22,9 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api', indexRouter)
 // catch 404 and forward to error handler
-// sequelize.query('SET FOREIGN_KEY_CHECKS = 0').then(function () {
-//   sequelize.sync({ alter: true })
-// })
+sequelize.query('SET FOREIGN_KEY_CHECKS = 0').then(function () {
+  sequelize.sync({ alter: true })
+})
 
 app.listen(5004)
 module.exports = app
