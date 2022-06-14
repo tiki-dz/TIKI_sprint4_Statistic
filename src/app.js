cd2725a550f9dc63fe1d9decbc9c66860c50fe78
nour <grebici.nour@gmail.com>
2022-06-14 23:00:36 +0100

    going online

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const { sequelize } = require('./models')
+// const { sequelize } = require('./models')
 // const createError = require("http-errors")
 const express = require('express')
 const path = require('path')
@@ -26,9 +26,9 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api', indexRouter)
 // catch 404 and forward to error handler
-sequelize.query('SET FOREIGN_KEY_CHECKS = 0').then(function () {
-  sequelize.sync({ alter: true })
-})
+// sequelize.query('SET FOREIGN_KEY_CHECKS = 0').then(function () {
+//   sequelize.sync({ alter: true })
+// })
 
 app.listen(5004)
 module.exports = app
